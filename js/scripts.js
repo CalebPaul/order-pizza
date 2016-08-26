8b42f11ab076375f9d88f02b2b884b5ff8508f2c
Caleb Paul <calebpaul21@gmail.com>
2016-08-26 08:11:48 -0700

    add Order constructor to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 //Business Logic
+function Order(name, size, toppings[], delivery, notes){
+  this.name = name;
+  this.size = size;
+  this.toppings = toppings;
+  this.delivery = true;
+  this.notes = notes;
+}
+
+Order.prototype.orderCost = function(price){
+  
+}
 
 //User Logic
 $(document).ready(function() {
