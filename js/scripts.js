8256c0b905599f1dd795665d4251682b57d7e521
Caleb Paul <calebpaul21@gmail.com>
2016-08-26 09:51:52 -0700

    add jquery captures for html forms and log to console for testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,29 @@
 //Business Logic
-function Order(name, size, toppings[], delivery, notes){
+function Order(name, size, toppings, delivery){
   this.name = name;
   this.size = size;
   this.toppings = toppings;
   this.delivery = true;
-  this.notes = notes;
 }
 
 Order.prototype.orderCost = function(price){
-  
+  price = 10;
+
 }
 
 //User Logic
 $(document).ready(function() {
-
+  $("form").submit(function(event) {
     event.preventDefault();
-
+    var newName = $("#order_name").val();
+    console.log(newName);
+    var newSize = $("input:radio[name=size]:checked").val();
+    console.log(newSize);
+    var newToppings = $("input:checkbox[name=toppings]:checked").val();
+    console.log(newToppings);
+    var newDeliv = $("input:radio[name=delivery]:checked").val();
+    console.log(newDeliv);
+    var newOrder = new Order(newName, newSize, newToppings, newDeliv)
+    console.log(newOrder);
+  })
 });
