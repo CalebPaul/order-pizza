08527fb55abe55e1b5ac16ab2dd2c7a945329786
Caleb Paul <calebpaul21@gmail.com>
2016-08-26 11:17:09 -0700

    remove nested array in toppings property that was causing error in js file, add working price if then statement based on toppings

--- js/scripts.js
+++ js/scripts.js
@@ -7,26 +7,41 @@ function Order(name, size, toppings, delivery){
 }
 
 Order.prototype.orderCost = function(price){
-  price = 10;
-
+  if (this.size === "slice"){
+    price -= 8;
+  } else if (this.size === "large") {
+    price += 4;
+  }
+  if (this.toppings.length > 0) {
+    price += (this.toppings.length / 2);
+  }
+  console.log("length: "+this.toppings.length);
+  console.log("price: "+price);
+  return price;
 }
 
 //User Logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
+    var price = 12;
     var newName = $("#order_name").val();
     console.log(newName);
     var newSize = $("input:radio[name=size]:checked").val();
     console.log(newSize);
-
-    var newToppings = new Array($("input:checkbox[name=toppings]:checked").map(function () {
+    var newToppings = $("input:checkbox[name=toppings]:checked").map(function () {
       return this.value;
-    }).get());
+    }).get();
     console.log(newToppings);
+
+    // console.log("array length "+parseInt(Object.keys(newToppings).length));
+
     var newDeliv = $("input:radio[name=delivery]:checked").val();
     console.log(newDeliv);
     var newOrder = new Order(newName, newSize, newToppings, newDeliv)
     console.log(newOrder);
+
+
+    newOrder.orderCost(price);
   })
 });
---
+++ notes.md
@@ -0,0 +1,16 @@
+# Order Pizza
+Create a website for a pizza company where a user can choose one or more individual toppings (cheese, pepperoni, artichoke, anchovy, etc) and a size to order a pizza and see the final cost.
+
+* Allow the user to choose toppings and size for the pizza they'd like to order.
+* Create a pizza object constructor with properties for toppings and size.
+* Create a prototype method for the cost of a pizza depending on the selections chosen. Use your own formula for this.
+
+# Your code will be reviewed for the following objectives:
+
+* Code meets standards from previous weeks.
+* Objects drive business logic (in JavaScript, not jQuery).
+* Constructors and prototypes are used successfully.
+* Application works as expected.
+* Plain English specs are included in README.
+* Project is in a polished, portfolio-quality state.
+* You are able to discuss the flow of your code and concepts behind it using correct terminology.
